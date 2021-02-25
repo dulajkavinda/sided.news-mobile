e234c29dd0555aae9e507dc2988d000e1fe1ec39
Dulaj Kavinda <dulajkaavinda@gmail.com>
2021-02-25 21:27:15 +0530

    initial commit

--- App.js
+++ App.js
@@ -1,21 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { WebView } from "react-native-webview";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <WebView source={{ uri: "https://sided.news" }} style={{ marginTop: 20 }} />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
